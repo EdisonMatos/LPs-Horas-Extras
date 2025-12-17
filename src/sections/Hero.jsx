e0e478c87482b9a9e23c835fb00215e6c7e50156
Edison Matos <edison.matos@live.com>
2025-12-17 16:48:52 -0300

    feat: Finaliza hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -79,12 +79,13 @@ export default function Hero({ imagemFocadaBackground, colorMode }) {
             <p
               className={`max-w-3xl mx-auto font-bold text-primary mt-6 mb-4 text-lg sm:text-xl text-fond/85 font-secondFont ${subtitleColor} text-[14px] phone2:text-[16px] phone3:text-[18px] lg:text-[20px]`}
             >
-              ⚠️ Você tem direitos a receber se:
+              ⚠️ Você tem indenização a receber se:
             </p>
             <ul className="gap-1 flex flex-col text-[12px] phone2:text-[14px] phone3:text-[16px] lg:text-[18px]">
               <li>✓ Você trabalhava em mais de uma função</li>
               <li>✓ Não recebia todas as horas extras</li>
               <li>✓ Trabalhou período sem carteira assinada</li>
+              <li>✓ Entre outros direitos violados</li>
             </ul>
           </div>
           <div className="flex flex-col items-center justify-center mt-10 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
